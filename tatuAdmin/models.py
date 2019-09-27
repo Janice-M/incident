4fe868875a04f050be5979372cddef92d2bf192a
Sunday Brian <brians931@gmail.com>
2019-09-27 21:52:35 +0300

    add a ticket type model

--- tatuAdmin/models.py
+++ tatuAdmin/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from PIL import Image
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class TicketType(models.Model):
+    Critical = 1
+    High = 2
+    Normal = 3
+    Low = 4
+    Very_Low=5
+
+
+    PRIORITY_CHOICES=(
+        (Critical,'1. Critical'),
+        (High,'2. High'),
+        (Normal,'3. Normal'),
+        (Low,'4. Low'),
+        (Very_Low,'5. Very Low'),
+    )
+
+    name=models.CharField(max_length=15)
+    priority=models.IntegerField(
+        choices=PRIORITY_CHOICES,
+        default=3,
+        blank=3,
+        
+    )
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_ticket_types(cls):
+        tickets=cls.objects.all()
+        return tickets  
+
+    @classmethod
+    def delete_ticket(cls,pk):
+        ticket=cls.objects.get(pk=pk)
+        ticket.delete()
+             
