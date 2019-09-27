10da54f828c08d32176ac3b339989a4b0c6e5150
Janice-M <janiceink001@gmail.com>
2019-09-27 16:45:45 +0300

    registration views

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm,UserUpdateForm,ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def register(request):
+    '''
+    view function for registering 
+    '''
+    if request.method=='POST':
+        form=UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account for {username} created!')
+            return redirect('login')
+
+    else:
+        form=UserRegistrationForm()
+    
+    return render(request,'registration/registration_form.html',{'form':form})
