62eef125ed0bcdc0c7004f6190ed8d93eab02f75
Sunday Brian <brians931@gmail.com>
2019-09-27 22:23:12 +0300

    create form to create an agent

---
+++ tatuAdmin/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from phonenumber_field.formfields import PhoneNumberField
+from django.db import models
+from .models import *
+from customer.models import Profile
+
+
+class AgentCreationForm(UserCreationForm):
+    email=forms.EmailField()
+
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
