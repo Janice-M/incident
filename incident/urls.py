05b302e54a46aa707b8b0772ce68a55832b3fd63
Sunday Brian <brians931@gmail.com>
2019-09-27 22:02:46 +0300

    register the tatuadmin url in  the projects urls

--- incident/urls.py
+++ incident/urls.py
@@ -26,13 +26,12 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',customer_views.index,name='index'),
     path('accounts/register/',customer_views.register,name='register'),
     path('accounts/login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
     path('accounts/logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
-    path('',customer_views.index,name='index'),
-    
-    
-    
+    path('tatuadmin/',include('tatuAdmin.urls')),
+     
     
 ]
 
