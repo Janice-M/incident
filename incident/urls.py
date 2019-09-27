b01324f5bf649cee32b1c9fe0008a1b12be74bd4
Janice-M <janiceink001@gmail.com>
2019-09-27 17:05:49 +0300

    logout url in incident urls.py

--- incident/urls.py
+++ incident/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/register/',customer_views.register,name='register'),
     path('accounts/login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
+    path('accounts/logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
+    
+    
     
     
 ]
