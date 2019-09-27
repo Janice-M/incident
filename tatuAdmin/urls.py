e261e81303a93b5669be9c891ad10c3eda2c6d27
Sunday Brian <brians931@gmail.com>
2019-09-27 22:33:04 +0300

    create url for edit agent view function

--- tatuAdmin/urls.py
+++ tatuAdmin/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',views.admin_home,name='admin_home'),
     path('user_management/',views.user_management,name='user_management'),
     path('user_management/create_agent',views.create_agent,name='create_agent'),
+    path('user_management/<int:pk>/edit_agent',views.edit_agent,name='edit_agent'),
 ]
 
