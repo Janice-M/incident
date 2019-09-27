1f81f8d6f800660d8e41055e81231c981ba52b0b
Janice-M <janiceink001@gmail.com>
2019-09-27 17:00:33 +0300

    registration url incident project

--- incident/urls.py
+++ incident/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from customer import views as customer_views
+
 from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/register/',customer_views.register,name='register'),
+    
 ]
 
 
