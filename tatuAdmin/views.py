7483148fe82191e6fa1fbd1b134dcd03d7b3f36c
Sunday Brian <brians931@gmail.com>
2019-09-27 22:08:18 +0300

    add imports to the tatuadmin views.py

--- tatuAdmin/views.py
+++ tatuAdmin/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import *
+from .models import *
+from django.views.generic import (UpdateView,DeleteView)
+from django.contrib.auth.mixins import (LoginRequiredMixin,UserPassesTestMixin)
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
