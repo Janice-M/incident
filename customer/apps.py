9a8efbe7d8bd7fbaf1f8d8fd79910ac1b7757fc1
Janice-M <janiceink001@gmail.com>
2019-09-27 16:49:45 +0300

    signal in apps.py

--- customer/apps.py
+++ customer/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class CustomerConfig(AppConfig):
     name = 'customer'
+    def ready(self):
+        print('signal is ready grey')
+        import customer.signals
