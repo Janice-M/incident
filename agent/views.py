330398d5f3750e51c3ca20c1bae8d116bf35e327
JOFLIX <joflixooko@outlook.com>
2019-09-28 14:46:55 +0300

    create the appendable view function for tickets

--- agent/views.py
+++ agent/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from tatuAdmin import views as tatuAdmin_views
+from customer.models import Create_ticket
 # Create your views here.
+@login_required
+def index(request):
+    tickets = Create_ticket.get_tickets()
+    return render(request, 'index.html' ,{'tickets' : tickets })
