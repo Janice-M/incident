a9628437d711dfa3c97045078b8d47bf7d60cf76
Michelle Mukami <kamimello463@gmail.com>
2019-09-28 10:33:08 +0300

    method made to get_ticket

--- customer/models.py
+++ customer/models.py
@@ -65,3 +65,9 @@ class Create_ticket(models.Model):
 
     def __str__(self):
         return f'{self.owner.username}{self.issue}'
+
+    @classmethod
+    def get_my_tickets(cls,owner):
+        my_tickets=cls.objects.filter(owner=owner).all()
+         return my_tickets
+
