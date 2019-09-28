76ee0a332e5028819c0dd0f78aa6a8b1a7628251
JOFLIX <joflixooko@outlook.com>
2019-09-28 14:41:12 +0300

    routing the index page

---
+++ agent/urls.py
@@ -0,0 +1,12 @@
+
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    # path('create_ticket/',views.create_ticket,name='createticket'),
+    # path('profile/',views.profile,name='profile'),
+]
