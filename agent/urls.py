bd5d8086adf9cb5198fd7f9f17b1b1bcd011b735
JOFLIX <joflixooko@outlook.com>
2019-09-29 11:15:40 +0300

    redifined the urls to include the agent's landfing page

--- agent/urls.py
+++ agent/urls.py
@@ -6,9 +6,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.agent_home,name='agent_home'),
     path('take_or_assign_ticket/<int:pk>/',views.take_or_assign_ticket,name='take_or_assign_ticket'),
     path('my_tickets/',views.my_tickets,name='my_tickets'),
-    # path('profile/',views.profile,name='profile'),
     # path('profile/',views.profile,name='profile'),
 ]
