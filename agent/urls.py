e27249a2165aa4d22a9bc22a182fada7dcdf527b
JOFLIX <joflixooko@outlook.com>
2019-09-29 10:27:20 +0300

    routing my_tickets view

--- agent/urls.py
+++ agent/urls.py
@@ -8,5 +8,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('take_or_assign_ticket/<int:pk>/',views.take_or_assign_ticket,name='take_or_assign_ticket'),
+    path('my_tickets/',views.my_tickets,name='my_tickets'),
+    # path('profile/',views.profile,name='profile'),
     # path('profile/',views.profile,name='profile'),
 ]
