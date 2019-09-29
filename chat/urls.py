832186bc8b3da28587f591a3328dc005074a40bc
JOFLIX <joflixooko@outlook.com>
2019-09-29 04:43:18 +0300

    configuring the routing of our api chat

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,11 @@
-"""URL's for the chat app."""
-
-from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import (
+	ChatSessionView, ChatSessionMessageView
+)
 
 urlpatterns = [
-    path('chats/', views.ChatSessionView.as_view()),
-    path('chats/<uri>/', views.ChatSessionView.as_view()),
-    path('chats/<uri>/messages/', views.ChatSessionMessageView.as_view()),
+	path('chats/', ChatSessionView.as_view()),
+	path('chats/<uri>/', ChatSessionView.as_view()),
+	path('chats/<uri>/messages/', ChatSessionMessageView.as_view()),
 ]
