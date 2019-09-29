1febc43924aa541cfb61a2206cc0253d80a6733d
JOFLIX <joflixooko@outlook.com>
2019-09-29 05:51:48 +0300

    implementing the notifications

--- chat-frontend/src/router/index.js
+++ chat-frontend/src/router/index.js
@@ -7,16 +7,12 @@ Vue.use(Router)
 
 const router = new Router({
   routes: [
-    // {
-    //   path: '/chats',
-    //   name: 'Chat',
-    //   component: Chat
-    // },
     {
-        path: '/chats/:uri?',
-        name: 'Chat',
-        component: Chat
+      path: '/chats/:uri?',
+      name: 'Chat',
+      component: Chat
     },
+
     {
       path: '/auth',
       name: 'UserAuth',
@@ -28,7 +24,6 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   if (sessionStorage.getItem('authToken') !== null || to.path === '/auth') {
     next()
-  }
   } else {
     next('/auth')
   }
