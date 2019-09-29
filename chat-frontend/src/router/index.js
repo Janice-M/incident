2bf4c1491b0d2b9052c14b62dfbaa74c17f258d9
JOFLIX <joflixooko@outlook.com>
2019-09-29 03:13:22 +0300

    implementing a global navigation

--- chat-frontend/src/router/index.js
+++ chat-frontend/src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Chat from '@/components/Chat'
+import UserAuth from '@/components/UserAuth'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/chats',
+      name: 'Chat',
+      component: Chat
+    },
+
+    {
+      path: '/auth',
+      name: 'UserAuth',
+      component: UserAuth
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (sessionStorage.getItem('authToken') !== null || to.path === '/auth') {
+    next()
+  }
+  } else {
+    next('/auth')
+  }
+})
+
+export default router
