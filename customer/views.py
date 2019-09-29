afe2d8aedb1dcfa1d4f19247980caa9c1b62d353
Sunday Brian <brians931@gmail.com>
2019-09-29 23:03:55 +0300

    add imports for the email activation

--- customer/views.py
+++ customer/views.py
@@ -11,6 +11,13 @@ from .generator import randomStringDigits
 from customer.models import Profile
 from django.core.exceptions import ObjectDoesNotExist
 
+from django.contrib.auth import login,authenticate
+from django.contrib.sites.shortcuts import (get_current_site)
+from django.utils.encoding import force_bytes,force_text
+from django.utils.http import (urlsafe_base64_encode, urlsafe_base64_decode)
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
 
 
 def register(request):
