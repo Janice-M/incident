6bca288aa7bb9cdcaccb52ff8ddaa10494f99de4
JOFLIX <joflixooko@outlook.com>
2019-09-29 12:19:56 +0300

    added resolve ticket urls

--- agent/urls.py
+++ agent/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls import url
 from django.urls import path
 from . import views
@@ -9,5 +8,6 @@ urlpatterns = [
     path('',views.agent_home,name='agent_home'),
     path('take_or_assign_ticket/<int:pk>/',views.take_or_assign_ticket,name='take_or_assign_ticket'),
     path('my_tickets/',views.my_tickets,name='my_tickets'),
+    path('resolve_tickets/<int:pk>',views.resolve_ticket,name='resolve_ticket'),
     # path('profile/',views.profile,name='profile'),
 ]
