aa7119ba8dd1efd46d29531282c66099b72ea142
Janice-M <janiceink001@gmail.com>
2019-09-30 16:22:12 +0300

    tokens for agennt email 3

--- tatuAdmin/views.py
+++ tatuAdmin/views.py
@@ -10,6 +10,16 @@ from django.views.generic import (UpdateView,DeleteView)
 from django.contrib.auth.mixins import (LoginRequiredMixin,UserPassesTestMixin)
 from django.contrib.messages.views import SuccessMessageMixin
 
+
+from django.contrib.auth import login,authenticate
+from django.contrib.sites.shortcuts import (get_current_site)
+from django.utils.encoding import force_bytes,force_text
+from django.utils.http import (urlsafe_base64_encode, urlsafe_base64_decode)
+from django.template.loader import render_to_string
+from customer.tokens import account_activation_token
+from django.core.mail import EmailMessage
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+
 # Create your views here.
 @login_required
 def admin_home(request):
