f4c11ad65c8f1b3526cfcfffb15b7974376d188c
JOFLIX <joflixooko@outlook.com>
2019-10-02 19:38:47 +0300

    resolve redirecting bug

--- customer/urls.py
+++ customer/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('create_ticket/',views.create_ticket,name='createticket'),
-    path('agent_profile/',views.profile,name='agent_profile'),
+    path('profile/',views.profile,name='profile'),
     url(r'^search/$',views.search_results,name='search_results'),
 
 ]
