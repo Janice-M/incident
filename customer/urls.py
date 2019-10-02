7183c9d0a1981b395178d9e455f0d5a9fb27cea3
JOFLIX <joflixooko@outlook.com>
2019-10-02 19:23:33 +0300

    rectified the urls

--- customer/urls.py
+++ customer/urls.py
@@ -7,10 +7,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('create_ticket/',views.create_ticket,name='createticket'),
-    path('profile/',views.profile,name='profile'),
+    path('agent_profile/',views.profile,name='agent_profile'),
     url(r'^search/$',views.search_results,name='search_results'),
-    
+
 ]
-
-
-
