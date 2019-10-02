0ad7914fdd61449c4fae66a3be65e6b764e22ce6
Sunday Brian <brians931@gmail.com>
2019-10-02 19:40:53 +0300

    fix error for profile update and missing forgot password

--- agent/urls.py
+++ agent/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('my_tickets/',views.my_tickets,name='my_tickets'),
     path('resolve_tickets/<int:pk>',views.resolve_ticket,name='resolve_ticket'),
     # path('profile/',views.profile,name='profile'),
-    path('profile/',views.profile,name='profile'),
+    path('agent_profile/',views.profile,name='agent_profile'),
 ]
